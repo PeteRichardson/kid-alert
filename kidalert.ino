3bcd0c469a8492fdaf07ca5f10562cdc78167584
Pete Richardson <pete@peterichardson.com>
2013-05-15 12:12:03 -0700

    working version... needs improved phonemes

--- kidalert.ino
+++ kidalert.ino
@@ -1,8 +1,13 @@
 #include <SoftwareSerial.h>
 #include "Emic2TtsModule.h"
 
-int rxSpeech = 4;
-int txSpeech = 5;
+int pirPin   =  2;
+int rxSpeech =  4;
+int txSpeech =  5;
+int ledPin   = 13;
+
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
 
 SoftwareSerial emic2Serial = SoftwareSerial(rxSpeech,txSpeech);
 Emic2TtsModule emic = Emic2TtsModule(&emic2Serial);
@@ -10,10 +15,26 @@ Emic2TtsModule emic = Emic2TtsModule(&emic2Serial);
 void setup() {
   emic2Serial.begin(9600);
   emic.init();
-  emic.say("Katherine, go back to bed!");
+  emic.say("Sentry starting");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly: 
-  
+  val = digitalRead(pirPin);  // read input value
+  if (val == HIGH) {            // check if the input is HIGH
+    digitalWrite(ledPin, HIGH);  // turn LED ON
+    if (pirState == LOW) {
+      // we have just turned on
+      emic.say("hello");
+      // We only want to print on the output change, not state
+      pirState = HIGH;
+    }
+  } else {
+    digitalWrite(ledPin, LOW); // turn LED OFF
+    if (pirState == HIGH){
+      // we have just turned of
+      emic.say("goodbye");
+      // We only want to print on the output change, not state
+      pirState = LOW;
+    }
+  }
 }
